e9f2117f2d172afb1fab69401c980b34ebc0a928
Islam Morse <i.morse@egyptunicom.com>
2019-06-19 16:31:58 +0200

    login page

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // start glabel
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
+    $('.datepicker').datepicker();
+    $('select').formSelect();
+});
+// start show password and hide
