d3a15f90da0ac530de49fd30804c16d9d8ca4142
Islam Morse <i.morse@egyptunicom.com>
2019-06-20 12:42:01 +0200

    dashboard page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,5 +4,24 @@ $(document).ready(function() {
     $('.dropdown-trigger').dropdown();
     $('.datepicker').datepicker();
     $('select').formSelect();
+
+
+
 });
 // start show password and hide
+
+// start show password and hide
+function showPassword() {
+    var prev = document.getElementById("password");
+    if (prev.type === "password") {
+        prev.type = "text";
+
+    } else {
+        prev.type = "password";
+
+    }
+}
+// start remove or reset data input
+function removeData() {
+    document.getElementById("myForm").reset();
+}
