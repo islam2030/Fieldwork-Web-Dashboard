297f8d9137f8275e15220095e8326dd2aa3f7478
islam2030 <islamessayed@gmail.com>
2019-06-21 23:02:44 +0200

    done without footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,22 @@ $(document).ready(function() {
     $('.dropdown-trigger').dropdown();
     $('.datepicker').datepicker();
     $('select').formSelect();
+    $('#datatable').DataTable();
 
-
+    // upload image
+    $(document).on("change", ".uploadFile", function() {
+        var uploadFile = $(this);
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return;
+        if (/^image/.test(files[0].type)) {
+            var reader = new FileReader();
+            reader.readAsDataURL(files[0]);
+            le
+            reader.onloadend = function() {
+                uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url(" + this.result + ")");
+            }
+        }
+    });
 
 });
 // start show password and hide
